4b6d31d8b5607fe218836de146abedeb79b63c25
Hazdryx <hazdryx@gmail.com>
2021-08-21 18:13:16 -0500

    Created element auto generation.

--- starlit/index.js
+++ starlit/index.js
@@ -2,11 +2,11 @@ const svgfs = require('./svgfs');
 const path = require('path');
 const starlit = require('./starlit');
 
-console.log('\nGenerating StarLit Background\n---------------------------------------');
+console.log('\nGenerating \x1b[35mStarLit\x1b[0m Background\n---------------------------------------');
 
 // Load template file.
 const templatePath = path.join(process.cwd(), 'starlit-temp.svg');
-console.log('Loading SVG template: ' + templatePath);
+console.log('\x1b[36mLoading SVG template\x1b[0m: ' + templatePath);
 let svg = svgfs.loadSvg(templatePath);
 
 // Starlit
@@ -14,7 +14,7 @@ svg = starlit(svg);
 
 // Save
 const starlitPath = path.join(process.cwd(), 'starlit.svg');
-console.log('Saving SVG file: ' + starlitPath);
+console.log('\x1b[36mSaving SVG\x1b[0m: ' + starlitPath);
 svgfs.saveSvg(starlitPath, svg);
 
-console.log('Finished :)\n');
+console.log('\n\x1b[32mFinished\x1b[0m\n');
